ff73fe93d62f503891206c2123fa3ea22efb89cf
Kim McCallum <kchaase@gmail.com>
2020-04-02 11:32:22 -0600

    ready for POST/DELETE additions

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config')
+const { NODE_ENV } = require('./config');
+const ArticlesService = require('./articles-service');
 
 const app = express();
 
@@ -14,6 +15,35 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get('/articles', (req, res, next) => {
+    const knexInstance = req.app.get('db');
+    ArticlesService.getAllArticles(knexInstance)
+        .then(articles => {
+            res.json(articles)
+        })
+        .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+    const knexInstance = req.app.get('db')
+    ArticlesService.getById(knexInstance, req.params.article_id)
+        .then(article => {
+            if (!article) {
+                return res.status(404).json({
+                    error: {message: `Article doesn't exist`}
+                })
+            }
+            res.json({
+                id: article.id,
+                title: article.title,
+                style: article.style,
+                content: article.content,
+                date_published: new Date(article.date_published),
+            })
+        })
+        .catch(next)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
